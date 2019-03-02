0b8bc4cbc722c0353c9e3b594159599b09e20e43
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-01 16:41:47 -0800

    Correct edit user form.

--- app.py
+++ app.py
@@ -231,6 +231,7 @@ def profile():
         g.user.location = form.location.data
         g.user.bio = form.bio.data
         g.user.header_image_url = form.header_image_url.data
+        g.user.image_url = form.image_url.data
 
         db.session.commit()
 
